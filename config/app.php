9561e8e82d63d941fa7a45281c4b4f657a191562
wizarot <wizarot@qq.com>
2018-08-19 16:42:57 +0800

    <修改> 前台用戶认证,使用laravel自带. dev模式增加debug bar方便调试(貌似没symfony的好用.)

--- config/app.php
+++ config/app.php
@@ -91,7 +91,7 @@ return [
     |
     */
 
-    'fallback_locale' => 'zh-CN',
+    'fallback_locale' => 'en',
 
     /*
     |--------------------------------------------------------------------------
@@ -208,6 +208,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        // Laravel Debugbar
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
---
+++ resources/lang/zh-CN/auth.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'Register' => '注册',
+    'Login' => '登录',
+    'E-Mail Address' => '电邮地址',
+    'Password' => '密码',
+    'Remember Me' => '记住我',
+    'Forgot Your Password?' => '忘记密码?',
+    'Reset Password' => '重置密码',
+    'Confirm Password' => '验证密码',
+    'Logout' => '登出',
+    'Name' => '用戶名',
+
+];
